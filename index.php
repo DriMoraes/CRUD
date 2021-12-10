2015018e801523a78e847de2738973491e7b3838
DriMoraes <89082377+DriMoraes@users.noreply.github.com>
2021-12-10 16:56:53 -0300

    CRUD

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    //header
+    include_once 'includs/header.php';
+?>
+
+<div class="row">
+    <div class="col s12 m6 push-m3">
+        <h3 class="light"> Clientes</h3>
+        <table class="striped">
+            <thead>
+                <tr>
+                    <th>Nome: </th>
+                    <th>Sobrenome: </th>
+                    <th>Email: </th>
+                    <th>Idade: </th>                    
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Adriele</td> 
+                    <td>Moraes</td> 
+                    <td>adriele@gmail.com</td> 
+                    <td>25</td>
+                    <td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+                    <td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <a href="adicionar.php" class="btn">Adicionar Cliente</a>
+    </div>
+</div>
+
+<?php
+//footer
+    include_once 'includs/footer.php';
+?>
+       
